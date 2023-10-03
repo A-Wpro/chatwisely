fcf139a20f1dc394ea56b5aa95a9e29e9193fd7d
A_W <36549997+A-Wpro@users.noreply.github.com>
2023-10-03 13:54:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Dash(__name__)
 server = app.server
 app.layout = html.Div("test---------")
 
-  if __name__ == '__main__':
-  app.run_server(debug = False)
+if __name__ == '__main__':
+app.run_server(debug = False)
