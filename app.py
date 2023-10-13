9040c31d82abe30f1c7fbf048977377e83ad9606
A_W <36549997+A-Wpro@users.noreply.github.com>
2023-10-13 14:40:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from dash import Dash, dcc, html
+import dash
+from dash import html
+from selenium import webdriver
 
 app = Dash(__name__)
 server = app.server
+@app.callback(
+    dash.dependencies.Output("website-content", "children"),
+    dash.dependencies.Input("fetch-button", "n_clicks")
+)
+def fetch_website(n_clicks):
+    if n_clicks:
+        # Set up a headless browser using Selenium
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        browser = webdriver.Chrome(options=options)
+        
+        # Fetch the website
+        browser.get("https://www.example.com")
+        
+        # Capture the page's final rendered HTML
+        rendered_html = browser.page_source
+        
+        browser.quit()
+        
+        # Return the rendered HTML - though be cautious about security concerns!
+        return html.Div(rendered_html, dangerouslySetInnerHTML={'__html': rendered_html})
+    return ""
+
 app.layout = html.Div([
-    html.Iframe(
-        src='https://chat.openai.com/')
+    html.H1("Server-Side Render External Website with Selenium"),
+    html.Button("Fetch Website Content", id="fetch-button"),
+    html.Div(id="website-content")
 ])
 
 if __name__ == '__main__':
