e09033f19b5f40aede5dc00260a0dbc9715440ce
A_W <36549997+A-Wpro@users.noreply.github.com>
2023-10-13 11:51:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,24 @@ from dash import Dash, dcc, html
 
 app = Dash(__name__)
 server = app.server
-app.layout = html.Div("test---------")
+layout = html.Div([
+    html.H1('Load Another Webpage'),
+    dcc.Input(id='url', type='text', placeholder='Enter a URL'),
+    html.Button('Load Webpage', id='load-webpage'),
+    html.Div(id='webpage-container'),
+])
+
+@app.callback(
+    Output('webpage-container', 'children'),
+    [Input('load-webpage', 'n_clicks')],
+    [State('url', 'value')],
+)
+def load_webpage(n_clicks, url):
+    if n_clicks is None or n_clicks == 0:
+        return None
+
+    iframe = html.Iframe(src=url, style={'width': '100%', 'height': '500px'})
+    return iframe
 
 if __name__ == '__main__':
   app.run_server(debug = False)
