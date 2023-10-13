a50ef4a900beca9ac7bf385285355ccc4f31aa15
A_W <36549997+A-Wpro@users.noreply.github.com>
2023-10-13 11:57:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from dash import Dash, dcc, html
 
 app = Dash(__name__)
 server = app.server
-layout = html.Div([
+app.layout = html.Div([
     dcc.Iframe(
         src='https://www.google.com',
         style={'width': '100%', 'height': '500px'}
