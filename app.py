fe08cb8cad204ed465aa5b6eb4ac11f4e1b20428
A_W <36549997+A-Wpro@users.noreply.github.com>
2023-10-13 14:55:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from dash import Dash, dcc, html,Output,Input
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=chrome_options)
+driver.get('http://www.google.com')
+print('test')
+driver.close()
 
 app = Dash(__name__)
 server = app.server
