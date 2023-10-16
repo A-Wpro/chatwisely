0f0a8b58ae32657e8d9d42c49536d4cf64a11399
A_W <36549997+A-Wpro@users.noreply.github.com>
2023-10-16 09:37:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from dash import Dash, dcc, html,Output,Input
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-try:
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-except:
-    driver = webdriver.Chrome(executable_path="/app/.chromedriver/bin/chromedriver", chrome_options=chrome_options)
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
 driver.get("https://chat.openai.com/")
 print(driver.page_source)
 print("Finished!")
