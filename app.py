223a5f651a6118df694ce503ba0dd7f4da904f1d
A_W <36549997+A-Wpro@users.noreply.github.com>
2023-10-20 11:20:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,14 @@
 from dash import Dash, dcc, html,Output,Input
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import os
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-service = Service(f"{os.environ.get("CHROMEDRIVER_PATH")}\chromedriver.exe")
-driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.get("https://chat.openai.com/")
-print(driver.page_source)
-print("Finished!")
-"""
+
 app = Dash(__name__)
 server = app.server
-@app.callback(
-    Output("website-content", "children"),
-    Input("fetch-button", "n_clicks")
-)
-def fetch_website(n_clicks):
-    if n_clicks:
-        # Set up a headless browser using Selenium
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        browser = webdriver.Chrome(options=options)
-        
-        # Fetch the website
-        browser.get("https://chat.openai.com/")
-        
-        # Capture the page's final rendered HTML
-        rendered_html = browser.page_source
-        
-        browser.quit()
-        
-        # Return the rendered HTML - though be cautious about security concerns!
-        return html.Div(rendered_html, dangerouslySetInnerHTML={'__html': rendered_html})
-    return ""
 
 app.layout = html.Div([
     html.H1("Server-Side Render External Website with Selenium"),
-    html.Button("Fetch Website Content", id="fetch-button"),
     html.Div(id="website-content")
 ])
 
 if __name__ == '__main__':
   app.run_server(debug = False)
-"""
+
